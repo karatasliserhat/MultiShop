34ff46b9309279cd1e37959fc9f03a749b8920b6
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-04-11 11:55:16 +0300

    ShoppinCarController oluşturuldu sepet ve sipariş özeti sayfalrı eklendi ,ContactController oluşturuldu ve iletişim sayfası eklendi bu sayfalara özel alanlar viewComponentse taşındı test ok

---
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "İletişim";
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Controllers/ShoppingCartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v3 = "Ürünler";
+            ViewBag.v2 = "Sepetim";
+            return View();
+        }
+    }
+}
