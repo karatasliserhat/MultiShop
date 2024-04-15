1519db4fe90a9fae58c2f3a52f865f111814f207
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-04-15 16:05:39 +0300

    UI client tarafında Öne çıkan Ürünler alanı dinamik hale getirildi test ok

--- Frontends/MultiShop.WebUI/ViewComponents/UILayoutDefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutDefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Shared.Services.Abstract;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutDefaultViewComponents
 {
     public class _FeatureProductsDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IProductReadApiService _productReadApiService;
+
+        public _FeatureProductsDefaultComponentPartial(IProductReadApiService productReadApiService)
         {
+            _productReadApiService = productReadApiService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = await _productReadApiService.GetListAsync("Products");
+            if (result is not null)
+            {
+                return View(result);
+            }
             return View();
         }
     }
