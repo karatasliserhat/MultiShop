6b6aa8a571a8f0f4c4ce49249b68e868c4d3f993
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-06-04 15:06:32 +0300

    Google cloud üzerine .net core Mvc üzerinden fotoğraf ekleme işlemi sağlandı test ok.

---
+++ Services/Image/MultiShop.Images.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Images.WebUI.Dal.Entities;
+using MultiShop.Images.WebUI.Services;
+
+namespace MultiShop.Images.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICloudStorageService _cloudStorageService;
+
+        public DefaultController(ICloudStorageService cloudStorageService)
+        {
+            _cloudStorageService = cloudStorageService;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ImageDrive imageDrive)
+        {
+            
+                if (imageDrive is not null)
+                {
+                imageDrive.SavedFileName = GenerateFileNameToSave(imageDrive.Photo.FileName);
+                imageDrive.SavedUrl = await _cloudStorageService.UploadFileAsync(imageDrive.Photo, imageDrive.SavedFileName);
+                }
+                return RedirectToAction("Default", "Index");
+            
+        }
+        private string? GenerateFileNameToSave(string incomingFileName)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(incomingFileName);
+            var extension = Path.GetExtension(incomingFileName);
+            return $"{fileName}-{DateTime.Now.ToUniversalTime().ToString("yyyyMMddHHmmss")}{extension}";
+        }
+
+    }
+}
---
+++ Services/Image/MultiShop.Images.WebUI/Services/GCSConfigOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Images.WebUI.Services
+{
+    public class GCSConfigOptions
+    {
+        public string GCPStorageAuthFile { get; set; }
+        public string GoogleCloudStorageBucketName { get; set; }
+    }
+}
